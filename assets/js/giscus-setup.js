a9641b018061ca87b58696b05b0524762ed973f5
fraarici <68994074+fraarici@users.noreply.github.com>
2026-03-10 11:06:09 +0000

    Deploying to gh-pages from @ fraarici/fraarici.github.io@f6053c416777d6a7c15c6e63129f5c20b895fa03 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "fraarici/fraarici",
+    "data-repo": "fraarici/fraarici.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
