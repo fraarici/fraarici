66481046d724732194166cde95c220eb2180afcc
fraarici <68994074+fraarici@users.noreply.github.com>
2026-03-10 19:54:29 +0000

    Deploying to gh-pages from @ fraarici/fraarici.github.io@f5c85a759a70397357a3dbda9c3558e2ec693693 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -412,11 +412,6 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
-            },},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
           title: 'project 2',
           description: "a project with a background image and giscus comments",
@@ -457,6 +452,11 @@ ninja.data = [{
           description: "another project with an image 🎉",
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
+            },},{id: "projects-veni-grant",
+          title: 'Veni Grant',
+          description: "The noncommutative geometry of sphere bundles",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/veni/";
             },},{id: "teachings-data-science-fundamentals",
           title: 'Data Science Fundamentals',
           description: "This course covers the foundational aspects of data science, including data collection, cleaning, analysis, and visualization. Students will learn practical skills for working with real-world datasets.",
