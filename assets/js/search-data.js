d7364e27a27a898be2eba6b5c966ff836e082f6e
fraarici <68994074+fraarici@users.noreply.github.com>
2026-03-17 20:44:34 +0000

    Deploying to gh-pages from @ fraarici/fraarici.github.io@6934b5a33c36e796edfcf3f747a8ddd1583b45c3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -39,7 +39,7 @@ ninja.data = [{
           },
         },{id: "nav-events",
           title: "events",
-          description: "",
+          description: "here you can find a list of upcoming and past events.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/events/";
